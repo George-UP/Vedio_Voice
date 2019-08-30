a033ff19e64ee9580b963d0f6db6391b7b92c241
Wesley <zxwios@hotmail.com>
2019-08-30 21:33:05 +0800

    - 传DoctorId

--- app/src/main/java/com/yemiekai/vedio_voice/fragments/DoctorListFragment.java
+++ app/src/main/java/com/yemiekai/vedio_voice/fragments/DoctorListFragment.java
@@ -3,6 +3,7 @@ package com.yemiekai.vedio_voice.fragments;
 import android.app.Fragment;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -12,19 +13,16 @@ import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.LinearLayoutManager;
-import androidx.recyclerview.widget.OrientationHelper;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.bumptech.glide.Glide;
 import com.bumptech.glide.request.RequestOptions;
 import com.google.gson.Gson;
-import com.yemiekai.vedio_voice.DoctorActivity;
 import com.yemiekai.vedio_voice.DoctorInfoActivity;
 import com.yemiekai.vedio_voice.R;
 import com.yemiekai.vedio_voice.utils.datas.AllDoctorsPreview;
 
 import java.util.ArrayList;
-import java.util.List;
 
 /**
  * 这个Fragment的布局是一个RecyclerView, 一个垂直滑动的列表, 每一行显示4个医生
@@ -44,9 +42,13 @@ public class DoctorListFragment extends Fragment
 	RecyclerView recyclerView;
 	LinearLayoutManager layoutManager;
 	static String BaseIP = "http://123.207.108.39:7001";
-	String DoctorID = "";
     public ImageButton mButton1;
 
+
+
+	public interface OnRecyclerViewClickListener {
+		void onItemClickListener(String DoctorId,View view);
+	}
 
 	@Override
 	public void onCreate(Bundle savedInstanceState)
@@ -75,6 +77,8 @@ public class DoctorListFragment extends Fragment
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
 	{
+
+		MyDoctorAdapter adapter = new MyDoctorAdapter();
 		// 加载/res/layout/目录下的fragment_book_detail.xml布局文件
 		View rootView = inflater.inflate(R.layout.fragment_doctors_list, container, false);
 		recyclerView = (RecyclerView) rootView.findViewById(R.id.fragment_doctors_recycler);
@@ -85,21 +89,41 @@ public class DoctorListFragment extends Fragment
 		recyclerView.setLayoutManager(layoutManager);
 
 		// 设置适配器
-		recyclerView.setAdapter(new MyDoctorAdapter());
-        mButton1 = (ImageButton) getActivity().findViewById(R.id.layout_doctors_button1);
-        mButton1.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(getActivity(),DoctorInfoActivity.class);
-                intent.putExtra("DoctorID",DoctorID);
-                startActivity(intent);
-            }
-        });
+		recyclerView.setAdapter(adapter);
+
+		adapter.setItemClickListener(new OnRecyclerViewClickListener() {
+			@Override
+			public void onItemClickListener(String DoctorId,View view) {
+				try {
+					Intent intent = new Intent(getActivity(), DoctorInfoActivity.class);
+					intent.putExtra("DoctorId", DoctorId);
+					startActivity(intent);
+				}catch (Exception e)
+				{
+					Log.d("tag",e.toString());
+				}
+			}
+		});
+
 
 		return rootView;
 	}
 
 	public class MyDoctorAdapter extends RecyclerView.Adapter<MyDoctorAdapter.ViewHolder> {
+
+		private OnRecyclerViewClickListener listener;
+		public void setItemClickListener(OnRecyclerViewClickListener itemClickListener) {
+			listener = itemClickListener;
+		}
+
+		public String getDoctorId(int position){
+			String DoctorId = "";
+
+			int x = getX();
+			DoctorId = allDoctorsPreviews.get(x).getRows().get(position).getId();
+
+			return  DoctorId;
+		}
 
 		class ViewHolder extends RecyclerView.ViewHolder {
             ImageButton mButton1;
@@ -107,10 +131,20 @@ public class DoctorListFragment extends Fragment
 
 			public ViewHolder(View itemView) {
 				super(itemView);
+
 				mButton1 = itemView.findViewById(R.id.layout_doctors_button1);
 				mTextView1 = itemView.findViewById(R.id.layout_doctors_text1);
 
+				mButton1.setOnClickListener(new View.OnClickListener() {
+					@Override
+					public void onClick(View view) {
+							listener.onItemClickListener(getDoctorId(getAdapterPosition()),view);
+					}
+				});
+
+
 			}
+
 		}
 
 		public MyDoctorAdapter(){
@@ -127,13 +161,7 @@ public class DoctorListFragment extends Fragment
 		@Override
 		public void onBindViewHolder(@NonNull ViewHolder holder, final int position) {
 
-            int x = 0;
-            int y = second_category_index;
-            int num = 0;
-            for (int i = 0;i < first_category_index;i++){
-                x += mChild.get(i);
-            }
-            x += y;
+            int x = getX();
 
             AllDoctorsPreview.RowsBean Doctor = allDoctorsPreviews.get(x).getRows().get(position);
 
@@ -142,10 +170,8 @@ public class DoctorListFragment extends Fragment
 				urlcheck = urlcheck.replace('\\', '/');
 				String url = BaseIP + urlcheck;
 				RequestOptions options = new RequestOptions()
-//                        .override(174,255)
 						.override(180,285)
                         .fitCenter();
-//                Glide.with(fragment).load(url).into(holder.mButton1);
 				Glide.with(getActivity())
                         .load(url)
                         .apply(options)
@@ -162,36 +188,28 @@ public class DoctorListFragment extends Fragment
 
 			holder.mTextView1.setText(text);  // 设置文字
 
-            DoctorID = Doctor.getId();
-
-
-
-			// todo:
-			//      设置每个按钮的点击响应时间, 切换fragment, 显示医生详细信息
-			//      切换时要传入当前fragment的科室索引, 在客户按下"返回"按钮时要切换回现在的fragment
-			//      (因为切换了fragment后, 现在的fragment好像被destroy了)
 		}
 
-		@Override
-		public int getItemCount() {
-			/*
-			 * todo 这是设置列表的长度。  列表一行有4个医生
-			 *		到时候根据多少医生, 返回值为 (医生总数/4)
-			 * */
+		public int getX(){
 			int x = 0;
 			int y = second_category_index;
-			int num = 0;
 			for (int i = 0;i < first_category_index;i++){
 				x += mChild.get(i);
 			}
 			x += y;
-			num = allDoctorsPreviews.get(x).getCount();
+			return x;
+		}
+
+		@Override
+		public int getItemCount() {
+			int x = getX();
+			int num = allDoctorsPreviews.get(x).getCount();
             return num;
 		}
 
 		@Override
-		public long getItemId(int arg0) {
-			return arg0;
+		public long getItemId(int position) {
+			return position;
 		}
 
 	}
